8871d09fdcbebb01a047e90a4972462d865d0045
ArkadiuszNykiel <115628264+ArkadiuszNykiel@users.noreply.github.com>
2024-11-26 23:36:20 +0100

    Add files via upload
    fixed a bug with app.js

--- App.js
+++ App.js
@@ -3,18 +3,18 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Navbar from './Navbar';
 import Countries from './Countries';
+import CountryDetails from './CountryDetails';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
-        <div className="routes-container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/countries" element={<Countries />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/countries" element={<Countries />} />
+          <Route path="/countries/:id" element={<CountryDetails />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
